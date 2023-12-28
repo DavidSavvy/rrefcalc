e562210469856dc3382afea07427081c00b2d400
DavidSavvy <dspuzzling@gmail.com>
2023-12-27 22:28:54 -0600

    special cases partly done

--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@
 using namespace std;
 
 bool valdiateNumericInput(string input);
-void displayRREF(const vector<vector<int>>& rrefMatrix);
-void addRows(vector<vector<int>>& matrix, int targetRow, int rowToAdd, int scalar);
-bool isZeroRow(vector<vector<int>> matrix, int rowNumber);
-void multiplyRow(vector<vector<int>>& matrix, int rowNumber, int scalar);
-void swapRows(vector<vector<int>>& matrix, int numRow, int numRow2);
+void displayRREF(const vector<vector<double>>& rrefMatrix);
+void addRows(vector<vector<double>>& matrix, int targetRow, int rowToAdd, double scalar);
+bool isZeroRow(vector<vector<double>> matrix, int rowNumber);
+void multiplyRow(vector<vector<double>>& matrix, int rowNumber, int scalar);
+void swapRows(vector<vector<double>>& matrix, int numRow, int numRow2);
 bool promptForRetry();
 int promptForInteger(string prompt);
-void toRREF(vector<vector<int>>& matrix);
-void normalizeRow(vector<vector<int>>& matrix, int rowNumber, int pivot);
-int findPivot(vector<vector<int>>& matrix, int colNumber);
-void zeroAboveAndBelow(vector<vector<int>>& matrix, int rowNumber, int colNumber);
+void toRREF(vector<vector<double>>& matrix);
+void normalizeRow(vector<vector<double>>& matrix, int rowNumber, double pivotValue);
+int findPivot(vector<vector<double>>& matrix, int rowNumber);
+int findNextPivot(vector<vector<double>>& matrix, int colNumber);
+void zeroAboveAndBelow(vector<vector<double>>& matrix, int rowNumber, int colNumber);
 
 int main(){
     // This is a test of the DisplayRREF function:
     //vector<vector<int>> h = {{9,-2,-1,26},{-8,-1,-4,-5},{-5,-1,-2,-3}};
-    //vector<vector<int>> h = {{9,-2,-1},{-8,-1,-4},{-5,-1,-2}};
-    vector<vector<int>> h = {{1,2,1},{-2,-3,1},{3,5,0}};
+    //vector<vector<double>> h = {{9,-2,-1},{-8,-1,-4},{-5,-1,-2}};
+    vector<vector<double>> h = {{0,2,1},{-2,0,1},{3,5,0}};
     toRREF(h);
     displayRREF(h);
     // This is the end of the test.
@@ -31,11 +32,11 @@ int main(){
 
 // Display a matrix that is already in RREF form.
 // Expects the matrix to be in RREF form.
-void displayRREF(const vector< vector<int> >& rrefMatrix) {
+void displayRREF(const vector<vector<double>>& rrefMatrix) {
     cout << "The Reduced Row Echelon Form is:" << endl;
-    for (const vector<int>& row : rrefMatrix) {
-        for (const int& value : row) {
-            cout << " " << value;
+    for (const vector<double>& row : rrefMatrix) {
+        for (const double& value : row) {
+            cout << " " << value;        
         }
         cout << endl;
     }
@@ -45,14 +46,22 @@ void displayRREF(const vector< vector<int> >& rrefMatrix) {
 // Finds pivot value (at col number)
 // Normalizes current row based on the pivot value found (to make pivot 1)
 // Zeroes the rows above and below the current row based on the pivot column 
-void toRREF(vector<vector<int>>& matrix) {
+void toRREF(vector<vector<double>>& matrix) {
     int numCols = matrix.at(0).size();
     int numRows = matrix.size();
     int pivot;
-    int pivotValue;
+    double pivotValue;
     for (int i = 0; i < numRows; i++) {
+        displayRREF(matrix);
         pivot = findPivot(matrix, i);
-        if (pivot == -1) {
+        if (pivot == -1 || pivot != i) {
+            if (i != (numRows - 1)) {
+                int nextPivot = findNextPivot(matrix, i);
+                swapRows(matrix, i, nextPivot);
+                //displayRREF(matrix);
+                i -= 1;
+                continue;
+            }
             continue;
         }
         pivotValue = matrix.at(i).at(pivot);
@@ -65,21 +74,24 @@ void toRREF(vector<vector<int>>& matrix) {
 // Goes row by row
 // If row is the row number with given, skips
 // Otherwise, runs algorithm 
-void zeroAboveAndBelow(vector<vector<int>>& matrix, int rowNumber, int colNumber) {
+void zeroAboveAndBelow(vector<vector<double>>& matrix, int rowNumber, int colNumber) {
     int numRows = matrix.size();
     int numCols = matrix.at(0).size();
     for (int i = 0; i < numRows; i++) {
         if (i == rowNumber) {
             continue;
         }   
+        if (isZeroRow(matrix, i)) {
+            continue;
+        }
         addRows(matrix, i, rowNumber, -(matrix.at(i).at(colNumber)));
     }
 }
 
 // Finds first non-zero column based on a given row
-int findPivot(vector<vector<int>>& matrix, int rowNumber) {
-    int numRows = matrix.size();
-    for (int i = 0; i < numRows; i++) {
+int findPivot(vector<vector<double>>& matrix, int rowNumber) {
+    int numCols = matrix.at(0).size();
+    for (int i = 0; i < numCols; i++) {
         if (matrix.at(rowNumber).at(i) != 0) {
             return i;
         }
@@ -87,16 +99,26 @@ int findPivot(vector<vector<int>>& matrix, int rowNumber) {
     return -1;
 }
 
+int findNextPivot(vector<vector<double>>& matrix, int colNumber) {
+    int numRows = matrix.size();
+    for (int i = 0; i < numRows; i++) {
+        if (matrix.at(i).at(colNumber) != 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 // Divides entire row by a pivot value
-void normalizeRow(vector<vector<int>>& matrix, int rowNumber, int pivot) {
+void normalizeRow(vector<vector<double>>& matrix, int rowNumber, double pivotValue) {
     int numCols = matrix.at(rowNumber).size();
     for (int i = 0; i < numCols; i++) {
-        matrix.at(rowNumber).at(i) /= pivot;
+        matrix.at(rowNumber).at(i) /= pivotValue;
     }
 }
 
 // Adds a row to a targetRow multiplied by a scalar
-void addRows(vector<vector<int>>& matrix, int targetRow, int rowToAdd, int scalar) {
+void addRows(vector<vector<double>>& matrix, int targetRow, int rowToAdd, double scalar) {
     int rowLength = matrix.at(targetRow).size();
     for (int i = 0; i < rowLength; i++) {
         matrix.at(targetRow).at(i) += (matrix.at(rowToAdd).at(i) * scalar);
@@ -104,7 +126,7 @@ void addRows(vector<vector<int>>& matrix, int targetRow, int rowToAdd, int scala
 }
 
 // Checks if entire row is zeroes
-bool isZeroRow(vector<vector<int>> matrix, int rowNumber) {
+bool isZeroRow(vector<vector<double>> matrix, int rowNumber) {
     int rowLength = matrix.at(rowNumber).size();
     bool isZero = true;
     for (int i = 0; i < rowLength; i++) {
@@ -116,15 +138,15 @@ bool isZeroRow(vector<vector<int>> matrix, int rowNumber) {
 }
 
 // Takes the 2d vector matrix as a reference
-void multiplyRow(vector<vector<int>>& matrix, int rowNumber, int scalar) {
+void multiplyRow(vector<vector<double>>& matrix, int rowNumber, int scalar) {
     int rowLength = matrix.at(rowNumber).size();
     for (int i = 0; i < rowLength; i++) {
         matrix.at(rowNumber).at(i) *= scalar;
     }
 }
 
-void swapRows(vector<vector<int>>& matrix, int numRow, int numRow2) {
-    vector<int> temp = matrix.at(numRow);
+void swapRows(vector<vector<double>>& matrix, int numRow, int numRow2) {
+    vector<double> temp = matrix.at(numRow);
     matrix.at(numRow) = matrix.at(numRow2);
     matrix.at(numRow2) = temp;
 }
